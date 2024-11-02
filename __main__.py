dbfbbb32aed634680970f4a466ccd8a7dbd7321c
TheRedCyclops <76230780+TheRedCyclops@users.noreply.github.com>
2024-11-02 23:13:50 +0100

    fix: everything

--- __main__.py
+++ __main__.py
@@ -1,45 +1,47 @@
 from wakeonlan import send_magic_packet
 from pexpect import pxssh
 from flask import Flask, request
+from time import sleep
 #import requests
 import os
 
 app = Flask(__name__)
 @app.route('/wol', methods=['POST'])
 def wol():
-    p = request.get_json()
     # send magic packet to wake up the server [WORKING]
-    wol_address = p['mac']
+    wol_address = request.args.get('mac')
     send_magic_packet(wol_address)
-    
+    # wait for the computer to turn on before trying to unlock it
+    timeout = int(request.args.get('timeout'))
+    sleep(timeout)
     # unlock the disk on the server
     s = pxssh.pxssh()
-    ssh_hostname = p['ip']
-    ssh_username = "root"
+    ssh_hostname = request.args.get('ip')
+    ssh_username = "theredcyclops"
     # using a key instead of password for ssh, read from default .ssh/
     # read the disk password from secret
     disk_password_file_name = os.environ['DISK_PASSWORD_FILE']
     disk_password_file = open(disk_password_file_name, "r")
     disk_password = disk_password_file.readline()
     disk_password_file.close()
-
+#
     # Log in to the server
     if not s.login (ssh_hostname, ssh_username):
         print("SSH session failed on login :(")
         print(str(s))
+        return "Failed"
     else:
         print("SSH session login successful")
     # Send the disk password to unlock the disk
         s.sendline (disk_password)
         s.prompt()         # match the prompt
         s.logout()
-    return '200'
-    ##    # Check the server is running
-    ##sleep(10)
-    ##if not requests.get('http://localhost:8000'):
-    ##    print("Server is down after WOL :(")
-    ##else:
-    ##    print(":)")
-        
+        return "Unlocked"
+    #    # Check the server is running
+    #sleep(10)
+    #if not requests.get('http://localhost:8000'):
+    #    print("Server is down after WOL :(")
+    #else:
+    #    print(":)")
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
